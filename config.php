e87b520ad26ac839f9a5af31c521f3ec58682aff
chiakiokumura <xxchiaxx@hotmail.co.jp>
2021-11-05 04:29:32 +0900

    データベース

--- config.php
+++ config.php
@@ -5,5 +5,10 @@ ini_set('display_errors', 1);
 date_default_timezone_set('Asia/Tokyo');
 //URLディレクトリ設定
 define('HOME_URL', '/mytwitter/');
+//データベースの接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'mytwitter');
 
 ?>
